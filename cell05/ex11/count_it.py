84ecff28c21825ad79df4cd739d3346f7fb7d04c
Dome <ktdd2557@gmail.com>
2026-09-24 17:56:20 +0700

    Add cell05 ex11

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+
+parameters = sys.argv[1:]
+
+if len(parameters) == 0:
+    print("none")
+else:
+    print(f"parameters: {len(parameters)}")
+
+    for parameter in parameters:
+        print(f"{parameter}: {len(parameter)}")
